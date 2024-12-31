8d0d8ba14f88948b28ebb735fc11264fb8754f8f
Lee Hyoju <168440628+Hyohyoju@users.noreply.github.com>
2025-01-01 00:01:09 +0900

    spring boot 기본 세팅 (AWS 키 때문에 후에 수정 예정)

---
+++ zupzup (1)/zupzup/src/test/java/com/MOA/zupzup/ZupzupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MOA.zupzup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupzupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
