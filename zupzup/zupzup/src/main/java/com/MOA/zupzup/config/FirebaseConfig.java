7725b4a4cb257c794ca0f38901337600a7fca2d1
Hyohyoju <julie08051@naver.com>
2025-01-07 22:43:41 +0900

    test: firebase 연동 테스트 성공

--- zupzup/zupzup/src/main/java/com/MOA/zupzup/config/FirebaseConfig.java
+++ zupzup/zupzup/src/main/java/com/MOA/zupzup/config/FirebaseConfig.java
@@ -3,13 +3,19 @@ package com.MOA.zupzup.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+@Configuration
 public class FirebaseConfig {
 
+    @PostConstruct
     public void initializeFirebase() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("path/to/serviceAccountKey.json");
+        FileInputStream serviceAccount = new FileInputStream("zupzup/zupzup/src/main/resources/zupzup-e3e05-firebase-adminsdk-1ujhj-e32eea045d.json");
 
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
