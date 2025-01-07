152af0b78116a5f31311538a623ae5e66dd77b82
Hyohyoju <julie08051@naver.com>
2025-01-07 18:33:24 +0900

    firebase 기본 세팅

---
+++ zupzup/zupzup/src/main/java/com/MOA/zupzup/config/FirebaseConfig.java
@@ -0,0 +1,20 @@
+package com.MOA.zupzup.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class FirebaseConfig {
+
+    public void initializeFirebase() throws IOException {
+        FileInputStream serviceAccount = new FileInputStream("path/to/serviceAccountKey.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        FirebaseApp.initializeApp(options);
+    }
+}
