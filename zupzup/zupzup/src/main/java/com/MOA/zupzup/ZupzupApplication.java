d8c48e959c344ad09cfca7be5ea7407db7e33b71
Hyohyoju <julie08051@naver.com>
2025-02-10 23:32:46 +0900

    Revert "Feat : Swagger"
    This reverts commit 662ef6b8adf509971e5d906b3d6b4a0f5fcf44df.

--- zupzup/zupzup/src/main/java/com/MOA/zupzup/ZupzupApplication.java
+++ zupzup/zupzup/src/main/java/com/MOA/zupzup/ZupzupApplication.java
@@ -3,10 +3,9 @@ package com.MOA.zupzup;
 import com.MOA.zupzup.login.FirebaseConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class ZupzupApplication {
 
     public static void main(String[] args) {
